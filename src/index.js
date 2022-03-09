d174a39013f1a7a381cec219132523fc8e728cb5
Luis Mendoza <luis.e.1998@hotmail.com>
2022-03-08 19:14:17 -0700

    header and schedule design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './css/styles.css';
+// Images
+import nurse from './assets/images/nurse.jpg';
+// Icons
+import star from './assets/icons/star.svg';
+import shield from './assets/icons/shield.svg';
+
+// Statements
+const imgNurse = document.querySelector('.img-nurse');
+
+const iconStar = document.querySelector('.icon-star');
+const iconShield = document.querySelector('.icon-shield');
+
+// Assigments
+imgNurse.src = `${nurse}`;
+
+iconStar.src = `${star}`;
+iconShield.src = `${shield}`;
